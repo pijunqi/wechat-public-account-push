11013d6317b085eaf163dbd2243f3f913e31a4b1
pijunqi <48879449+pijunqi@users.noreply.github.com>
2022-09-09 21:11:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,20 +18,20 @@ export const config = {
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "老婆0",
+   *    name: "老婆",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "oKX576K4mJxNXRXF9CPGbL7dqUAM",
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "viv3byWXd_z3aflBECZgP4HFvMdpNjWP1jjw3nlZm6k",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
+   *    openUrl: "https://www.cnblogs.com/pijunqi/"
    *   }, 
    * ]
    */
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKX576K4mJxNXRXF9CPGbL7dqUAM",
       // 你想对他发送的模板消息的模板ID
@@ -41,31 +41,15 @@ export const config = {
       // 所在城市
       city: "肇庆",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://www.cnblogs.com/pijunqi/"
     }, 
     {
-      name: "老婆1", 
+      name: "老公", 
       id: "oKX576K4mJxNXRXF9CPGbL7dqUAM",
       useTemplateId: "viv3byWXd_z3aflBECZgP4HFvMdpNjWP1jjw3nlZm6k",
       province: "",
       city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆2",
-      id: "oKX576K4mJxNXRXF9CPGbL7dqUAM",
-      useTemplateId: "viv3byWXd_z3aflBECZgP4HFvMdpNjWP1jjw3nlZm6k",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆3", 
-      id: "oKX576K4mJxNXRXF9CPGbL7dqUAM",
-      useTemplateId: "viv3byWXd_z3aflBECZgP4HFvMdpNjWP1jjw3nlZm6k",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://www.cnblogs.com/pijunqi/"
     }, 
   ],
 
@@ -93,9 +77,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "河南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "夏邑县",
 
   /** 重要节日相关 */
 
@@ -106,10 +90,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "美丽可爱的老婆大人", "year": "1994", "date": "04-22"},
+    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-16"},
+    {"type": "生日", "name": "帅气逼人的飒爽老公", "year": "1993", "date": "11-08"},
+    {"type": "节日", "name": "怀孕纪念日", "year": "2022", "date": "01-21"},
   ],
 
   /**
@@ -127,11 +111,11 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+    {"keyword": "love_day", date: "2017-05-20"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+    {"keyword": "marry_day", date: "2020-01-16"},
     // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    //{"keyword": "ex_day", date: "2022-08-31"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
