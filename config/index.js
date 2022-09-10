67677fda83ef71f5d0b63057a4b6dcf1d9c09a65
pijunqi <48879449+pijunqi@users.noreply.github.com>
2022-09-10 14:16:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,13 +6,13 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   APP_ID: "wx128d8fdd8e63b47e",
-
+  // APP_ID: "wx128d8fdd8e63b47e",
+APP_ID: "wx2ed6c75b4f03dd9c",
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   APP_SECRET: "2339e15872af457a4df19619123e2056",
-
+   // APP_SECRET: "2339e15872af457a4df19619123e2056",
+ APP_SECRET: "1dfe6a2e4b63c64f0a6925859b19814a",
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
    * [
